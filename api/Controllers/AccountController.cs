f9d58cbecdddb8a419d7942a3769ea503bf818ff
Anton Rum <antonrumancev67@gmail.com>
2025-02-06 23:41:31 +0300

    Added token service

--- api/Controllers/AccountController.cs
+++ api/Controllers/AccountController.cs
@@ -2,6 +2,7 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using api.Interfaces;
 using api.Models;
 using api.Models.Dto;
 using Microsoft.AspNetCore.Identity;
@@ -14,16 +15,19 @@ namespace api.Controllers
     public class AccountController : ControllerBase
     {
         private readonly UserManager<AppUser> _userManager;
+        private readonly ITokenService _tokenService;
 
-        public AccountController(UserManager<AppUser> userManager)
+        public AccountController(UserManager<AppUser> userManager, ITokenService tokenService)
         {
             _userManager = userManager;
+            _tokenService = tokenService;
         }
 
         [HttpPost("register")]
         public async Task<IActionResult> Register([FromBody] RegisterModel registerModel)
         {
-            try {
+            try
+            {
                 if(!ModelState.IsValid)
                 {
                     return BadRequest(ModelState);
@@ -37,17 +41,23 @@ namespace api.Controllers
 
                 var createdUser = await _userManager.CreateAsync(appUser, registerModel.Password);
 
-                if(!createdUser.Succeeded) {
+                if(createdUser.Succeeded)
+                {
                     var roleResult = await _userManager.AddToRoleAsync(appUser, "User");
                     if(roleResult.Succeeded)
                     {
-                        return Ok("User created");
-                    }else {
+                        return Ok(new NewUserModel {
+                            UserName = appUser.UserName,
+                            Email = appUser.Email,
+                            Token = _tokenService.CreateToken(appUser)
+                        });
+                    }else{
                         return StatusCode(500, roleResult.Errors);
                     }
-                } else {
+                }else {
                     return StatusCode(500, createdUser.Errors);
                 }
+
             } catch (Exception e) {
                 return StatusCode(500, e);
             }
