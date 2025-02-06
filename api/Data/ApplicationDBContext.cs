a36b2be614fde75ee6b10cdc380d77257e0fc426
Anton Rum <antonrumancev67@gmail.com>
2025-02-06 17:49:52 +0300

    Added Identity

---
+++ api/Data/ApplicationDBContext.cs
@@ -0,0 +1,18 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using api.Models;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+
+namespace api.Data
+{
+    public class ApplicationDBContext : IdentityDbContext<AppUser>
+    {
+        public ApplicationDBContext(DbContextOptions<ApplicationDBContext> dbContext) : base(dbContext)
+        {
+            
+        }
+    }
+}
