6354c571f726adf02a9f697715e4f3f0e3fb8888
Anton Rum <antonrumancev67@gmail.com>
2025-02-23 01:47:09 +0300

    Created context

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -2,14 +2,17 @@ import { Route, Routes } from 'react-router-dom';
 import './App.css';
 import RecipePage from './Pages/RecipePage';
 import RecipeInfoPage from './Pages/RecipeInfoPage';
+import { UserProvider } from './Context/userAuth';
 
 function App() {
   return (
     <div className="App">
-      <Routes>
-        <Route path="/" element={<RecipePage />} />
-        <Route path="/recipe/:id" element={<RecipeInfoPage />} />
-      </Routes>
+      <UserProvider>
+        <Routes>
+          <Route path="/" element={<RecipePage />} />
+          <Route path="/recipe/:id" element={<RecipeInfoPage />} />
+        </Routes>
+      </UserProvider>
     </div>
   );
 }
